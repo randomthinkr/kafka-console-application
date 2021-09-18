60100db2488bbc4257d02507a9295da750234a46
Buboy Navaja <buboyn@magenic.com>
2021-09-18 23:25:44 +0800

    initial commit for producer and consumer

---
+++ kafka-consumer/src/main/java/dev/csv/buboyn/consumer/KafkaConsumerApplication.java
@@ -0,0 +1,40 @@
+package dev.csv.buboyn.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.KafkaException;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.stream.IntStream;
+
+public class KafkaConsumerApplication {
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("group.id", "test-consumer-group");
+
+        //consume a message!
+        try (KafkaConsumer consumer = new KafkaConsumer(properties)) {
+            consumer.subscribe(List.of("myMultiReplicationFactorTopic"));
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.of(10, ChronoUnit.MILLIS));
+                records.forEach(record -> System.out.println("Message received: " + record.value()));
+            }
+
+        } catch (Exception e) {
+            //handle properly any exception thrown.
+            e.printStackTrace();
+        }
+    }
+}
---
+++ kafka-producer/src/main/java/dev/csv/buboyn/producer/KafkaProducerApplication.java
@@ -0,0 +1,34 @@
+package dev.csv.buboyn.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.KafkaException;
+
+import java.util.Properties;
+import java.util.stream.IntStream;
+
+public class KafkaProducerApplication {
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        //send a producerRecord (message)!
+        try(KafkaProducer producer = new KafkaProducer(properties)) {
+
+            IntStream.range(0, 20).forEach(i -> {
+                ProducerRecord<String, String> producerRecord =
+                        new ProducerRecord<>("myMultiReplicationFactorTopic", "Message: " + ++i);
+                producer.send(producerRecord);
+             });
+
+        }
+        catch (Exception e){
+            //handle properly any exception thrown.
+            e.printStackTrace();
+        }
+    }
+}
